684ae577a3c921b7b06c6224893ee1a143a4b9d1
Davinc3679 <ndubuisikingsley379@gmail.com>
2022-07-09 14:28:12 +0000

    5printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/*
+/**
  * main - entry point
  *
  * Return: 0 after printing function
@@ -10,6 +10,7 @@
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
+
 	return (0);
 }
 
