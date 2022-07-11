b870b1b499da1449fd0c4d6d6edc18d6d5e7b54b
Davinc3679 <ndubuisikingsley379@gmail.com>
2022-07-11 00:26:48 +0000

    nested functions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: prints 'Holberton \n'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char str[] = "_putchar\n";
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		char c = str[i];
+
+		_putchar(c);
+		i++;
+	}
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,4 @@
+0. _putchar
+ program that prints _putchar, followed by a new line.
+
+The program should return 0
