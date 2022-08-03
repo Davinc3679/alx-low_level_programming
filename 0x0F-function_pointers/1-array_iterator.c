27ac2d3082354d301a6dd60f2167193bd2e22438
Davinc3679 <ndubuisikingsley379@gmail.com>
2022-08-03 23:05:56 +0000

    final commit

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,7 +1,8 @@
 #include <stdlib.h>
 
 /**
- * array_iterator - a function that executes a function given as a parameter on each element of an array.
+ * array_iterator - a function that executes a function
+ * given as a parameter on each element of an array.
  * @array: A pointer to array
  * @size: Size of the array
  * @action: action(function) to iterate throw array
